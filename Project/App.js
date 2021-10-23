5fb791e7352ed619d34653c593cdeed1d2a2b682
qianyouwang <qianyou_wang@umail.ucsb.edu>
2021-10-22 22:26:42 -0700

    initial

--- Project/App.js
+++ Project/App.js
@@ -1,4 +1,6 @@
+// @refresh reset
 import * as React from 'react';
+import { useState, useEfffect } from 'react';
 import { View, Text, Button, StyleSheet, SafeAreaView } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
@@ -12,12 +14,41 @@ import CommunityScreen from './screens/CommunityScreen';
 import UserScreen from './screens/UserScreen';
 import LoginScreen from "./screens/LoginScreen";
 
+import * as firebase from "firebase";
+import 'firebase/firestore';
+
+// Firebase Configure
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGAPiJ4hblg4P4tbExbqdqZVDZKu7Dvw8',
+  authDomain: 'served-up-63c2e.firebaseapp.com',
+  databaseURL: 'https://served-up-63c2e.firebaseio.com',
+  projectId: 'served-up-63c2e',
+  storageBucket: 'served-up-63c2e.appspot.com',
+  //messagingSenderId: 'sender-id',
+  appId: '1:456652905966:ios:80d960e213cb40ea1182ff',
+  //measurementId: 'G-measurement-id',
+};
+
+// Initialize Firebase
+if (firebase.apps.length() === 0) {
+  firebase.initializeApp(firebaseConfig);
+
+}
 
 const Stack = createBottomTabNavigator();
 
 const App = () => {
+  const [user, setUser] = React.useState(null);
+  useEffect(() => {
+     readUser()
+  }, [])
+
+  async function readUser() {
+
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationCon tainer>
       <Stack.Navigator 
         initialRouteName="Login"
         screenOptions={({ route }) => ({
