67a3c3daca49a2f84ec45c04abef76f20e990f8b
Callter <quansen@ucsb.edu>
2021-11-06 14:50:45 -0700

    qs - update userscreen test

---
+++ Project/__tests__/LoginScreen.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// import App from '../App';
+// import CommunityScreen from '../screens/CommunityScreen';
+// import Loading from '../screens/Loading';
+import LoginScreen from '../screens/LoginScreen';
+// import MenuScreen from '../screens/MenuScreen';
+// import UserScreen from '../screens/UserScreen';
+
+describe('<App />', () => {
+  it('default render', () => {
+    renderer.create(<LoginScreen />);
+  });
+});
