f98a094e5031b90cb7afabb37eb65e46a65c16b9
Quansen Wang <quansen@ucsb.edu>
2021-11-06 14:48:40 -0700

    Update UserScreen.test.js

--- Project/__tests__/UserScreen.test.js
+++ Project/__tests__/UserScreen.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-
-// import App from '../App';
-// import CommunityScreen from '../screens/CommunityScreen';
-// import Loading from '../screens/Loading';
-// import LoginScreen from '../screens/LoginScreen';
-// import MenuScreen from '../screens/MenuScreen';
+import {useState as useStateMock} from 'react';
+import {MediaTypeOptions, requestMediaLibraryPermissionsAsync, launchImageLibraryAsync, launchCameraAsync} from 'expo-image-picker';
 import UserScreen from '../screens/UserScreen';
 
-describe('<App />', () => {
+jest.mock("expo-image-picker", () => ({
+  MediaTypeOptions: jest.fn(),
+  requestMediaLibraryPermissionsAsync: jest.fn(),
+  launchImageLibraryAsync: jest.fn(),
+  launchCameraAsync: jest.fn(),
+}));
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn(),
+}));
+
+describe('<UserScreen />', () => {
+  const setState = jest.fn();
+  beforeEach(() => {
+            useStateMock.mockImplementation(init => [init, setState]);
+         });
   it('default render', () => {
+    
     renderer.create(<UserScreen />);
   });
 });
