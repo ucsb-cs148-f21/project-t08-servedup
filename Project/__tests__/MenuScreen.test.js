e48b8afc8cb9e0fdaf1032bd7f1355c275ce6dee
Callter <quansen@ucsb.edu>
2021-11-10 00:30:42 -0800

    qs - add menu tests

--- Project/__tests__/MenuScreen.test.js
+++ Project/__tests__/MenuScreen.test.js
@@ -1,15 +1,56 @@
-import React from 'react';
 import renderer from 'react-test-renderer';
-
-// import App from '../App';
-// import CommunityScreen from '../screens/CommunityScreen';
-// import Loading from '../screens/Loading';
-// import LoginScreen from '../screens/LoginScreen';
+import * as React from 'react';
+import { useState } from 'react';
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: jest.fn()
+}));
+import { StyleSheet, View, Text, SafeAreaView, SectionList } from 'react-native';
+import SwitchSelector from "react-native-switch-selector";
+jest.mock("react-native-switch-selector", () => ({
+  SwitchSelector: jest.fn()
+}));
 import MenuScreen from '../screens/MenuScreen';
-// import UserScreen from '../screens/UserScreen';
 
 describe('<App />', () => {
+ const menuSpy = [
+  {
+    "name": "Tofu Banh Mi Sandwich (v)",
+    "station": "Entree Specials",
+  },
+  {
+    "name": "Pineapple Fried Rice (w/nuts) (vgn)",
+    "station": "Entree Specials",
+  }
+ ];
+ const fetchSpy = [
+    {
+      "_U": 0,
+      "_V": 0,
+      "_W": null,
+      "_X": null,
+    },
+    {
+      "_U": 0,
+      "_V": 0,
+      "_W": null,
+      "_X": null,
+    }
+ ];
+  // beforeEach(() => {
+  //   globalThis.fetch = jest.fn(() => Promise.resolve({
+  //     json: () => Promise.resolve(fetchSpy)
+  //   }));
+  // });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
   it('default render', () => {
+    globalThis.fetch = jest.fn(() => Promise.resolve({
+      json: () => Promise.resolve(menuSpy)
+    }));
+    const setResult = jest.fn();
+    jest.spyOn(React, 'useState').mockImplementation(results => [results=menuSpy, setResult]);
     renderer.create(<MenuScreen />);
   });
 });
