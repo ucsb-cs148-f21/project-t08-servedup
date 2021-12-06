2b248a76b87ec09384ed880c41bd326ef1eb54c8
Callter <quansen@ucsb.edu>
2021-12-06 14:56:19 -0800

    qs - delete unused files / add contrib

--- Project/setup-tests.js
+++
@@ -1,3 +0,0 @@
-import mockAsyncStorage from 'node_modules/@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('node_modules/@react-native-async-storage/async-storage', () => mockAsyncStorage);
